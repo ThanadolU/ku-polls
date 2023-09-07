459f00b850b1d0899b9cf4535944f9779e7abce2
ThanadolU <dolthanadolu@gmail.com>
2023-09-07 10:19:30 +0700

    modified views code to use can_vote and is_published and modified template to show messages

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.contrib import messages
 
 from .models import Question, Choice
 
@@ -30,6 +31,19 @@ class DetailView(generic.DetailView):
         Excludes any questions that aren't published yet.
         """
         return Question.objects.filter(pub_date__lte=timezone.localtime())
+    
+    def get(self, request, pk):
+        try:
+            question = Question.objects.get(pk=pk)
+        except Question.DoesNotExist as e:
+            raise Http404('Question does not exist') from e
+        if not question.is_published():
+            messages.error(request, 'This question is not published')
+            return HttpResponseRedirect(reverse('polls:index'))
+        if not question.can_vote():
+            messages.error(request, 'You cannot vote unpublished or ended question')
+            return HttpResponseRedirect(reverse('polls:index'))
+        return render(request, 'polls/detail.html', {'question': question})
 
 
 class ResultsView(generic.DetailView):
@@ -41,6 +55,13 @@ class ResultsView(generic.DetailView):
         Excludes any questions that aren't published yet.
         """
         return Question.objects.filter(pub_date__lte=timezone.localtime())
+    
+    def get(self, request, pk):
+        question = get_object_or_404(Question, pk=pk)
+        if not question.is_published():
+            messages.error(request, 'You cannot watch the result of unpublished or ended question')
+            return HttpResponseRedirect(reverse('polls:index'))
+        return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -49,9 +70,9 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
+        messages.error(request, "You didn't select a choice!")
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "You didn't select a choice."
         })
     else:
         selected_choice.votes += 1
