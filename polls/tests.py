79ed1f269e889618ac9d6844821bf68b228c0b80
ThanadolU <dolthanadolu@gmail.com>
2023-09-10 06:49:22 +0700

    added docstrign to new all new tests that i wrote

--- polls/tests.py
+++ polls/tests.py
@@ -55,37 +55,62 @@ class QuestionModelTests(TestCase):
         self.assertIs(recent_question.was_published_recently(), True)
 
     def test_is_published_for_question_in_the_future_pub_date(self):
+        """
+        is_published() return False means that you cannot vote.
+        """
         time = timezone.localtime() + datetime.timedelta(days=10)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.is_published(), False)
 
     def test_is_published_for_question_in_the_default_pub_date(self):
+        """
+        is_published() return True means that you can vote.
+        """
         current_question = Question(pub_date=timezone.localtime())
         self.assertIs(current_question.is_published(), True)
 
     def test_is_published_for_question_in_the_past_pub_date(self):
+        """
+        is_published() return True means that you can vote.
+        """
         time = timezone.localtime() - datetime.timedelta(days=10)
         past_question = Question(pub_date=time)
         self.assertIs(past_question.is_published(), True)
 
     def test_can_vote_for_question_in_future(self):
+        """
+        can_vote() returns False (voting is not allowed) 
+        because it is not pub_date.
+        """
         time = timezone.localtime() + datetime.timedelta(days=20)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.can_vote(), False)
 
     def test_can_vote_for_question_in_current(self):
+        """
+        can_vote() return True (voting is allowed) because pub_date 
+        and end_date are in the same date but different seconds.
+        """
         time = timezone.localtime()
         current_question = Question(pub_date=time, end_date=time+datetime.timedelta(seconds=1))
         self.assertIs(current_question.can_vote(), True)
 
     def test_can_vote_for_question_in_past(self):
+        """
+        can_vote() return False (voting is allowed) because 
+        current time is after end_date.
+        """
         time = timezone.localtime() - datetime.timedelta(days=-10)
         past_question = Question(pub_date=time, end_date=timezone.localtime())
         self.assertIs(past_question.can_vote(), False)
 
     def test_can_vote_with_no_end_date(self):
+        """
+        can_vote() return True (voting is allowed) for no end_date.
+        """
         question = Question(pub_date=timezone.localtime())
         self.assertIs(question.can_vote(), True)
+
 
 class QuestionIndexViewTests(TestCase):
 
@@ -189,7 +214,9 @@ class QuestionResultsViewTests(TestCase):
         self.assertContains(response, f'{choice.choice_text} -- {choice.votes}')
 
     def test_future_question_results_page(self):
-        """The results for unpublished questions should not be accessible."""
+        """
+        The results for unpublished questions should not be accessible.
+        """
         question = create_question(question_text='Test.', days=20)
         url = reverse('polls:results', args=(question.id, ))
         response = self.client.get(url)
