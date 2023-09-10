c8f15d84cb3ab40f9e74d33687577a5a00b31cee
ThanadolU <dolthanadolu@gmail.com>
2023-09-10 08:54:42 +0700

    finished decorate index and results pages for now

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', RedirectView.as_view(url='polls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
