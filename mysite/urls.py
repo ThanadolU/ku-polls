25f258c431d27ba7375008899dd9f8953e94b071
ThanadolU <dolthanadolu@gmail.com>
2023-09-12 15:02:30 +0700

    user can login and logout

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', RedirectView.as_view(url='polls')),
     path('polls/', include('polls.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
